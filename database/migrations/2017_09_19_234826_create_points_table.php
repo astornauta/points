1536b712aa9e3b44e5eb96e70d593d7a3330f252
Tincho <alvarezdmartinn@gmail.com>
2017-09-19 21:00:24 -0300

    Create migration files

---
+++ database/migrations/2017_09_19_234826_create_points_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePointsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('points', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('giver_id')->unsigned()->index();
+            $table->integer('receiver_id')->unsigned()->index();
+            $table->integer('quantity');
+            $table->string('reason');
+            $table->timestamps();
+            $table->index(['created_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('points');
+    }
+}
