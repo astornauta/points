270d248466151ef75194e6f37672da57b84e4c78
Tincho <alvarezdmartinn@gmail.com>
2017-09-19 21:27:06 -0300

    Fix user migration file

--- database/migrations/2017_09_19_233209_create_users_table.php
+++ database/migrations/2017_09_19_233209_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('first_name');
             $table->string('last_name');
+            $table->string('email')->unique()->index();
             $table->string('username')->unique()->index();
             $table->date('birth_date');
             $table->string('password', 60);
